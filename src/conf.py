3e2d83d1f289623e1fd756061c05ba78e62e2408
Francesco Zaia <francesco.zaia@gmail.com>
2018-11-07 14:53:55 +0100

    Testing md support

--- src/conf.py
+++ src/conf.py
@@ -1,8 +1,25 @@
 import os
 import yaml
+
+from recommonmark.transform import AutoStructify
+from recommonmark.parser import CommonMarkParser
+
 master_doc = 'index'
 html_theme = 'redirect_template'
 html_theme_path = ['.']
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+# -- AutoStructify --------------------------------------------------------
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+        'auto_toc_tree_section': 'Contents',
+        'enable_eval_rst': True,
+        'enable_auto_doc_ref': True
+    }, True)
+    app.add_transform(AutoStructify)
 
 try:
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),'redirect.yml')) as redirect:
